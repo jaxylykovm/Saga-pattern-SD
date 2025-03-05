76d0340a3b48f1ad9d8bd6eb6d4cfd94f4f49bd5
jaxylykovm <jaxylykovmagzhan@gmail.com>
2025-03-05 21:50:30 +0500

    saga_manager.py

--- saga_manager.py
+++ saga_manager.py
@@ -6,7 +6,7 @@ from payment_service import process_payment, compensate_payment
 from inventory_service import reserve_item, release_item
 from shipping_service import ship_order, cancel_shipment
 
-# Настраиваем логирование
+
 logging.basicConfig(level=logging.INFO, filename="saga.log", format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
 
@@ -23,20 +23,20 @@ async def process_order(order_id, item_id, quantity, amount):
     logger.info(f"🚀 Начинаем обработку заказа {order_id}...")
     await update_order_status(order_id, "processing")
 
-    # 1. Резервируем товар
+   
     if not await reserve_item(order_id, item_id, quantity):  
         logger.error(f"❌ Отмена заказа {order_id} (не хватает товара)")
         await update_order_status(order_id, "failed")
         return False
 
-    # 2. Обрабатываем оплату
+    
     if not await process_payment(order_id, amount):  
         logger.error(f"❌ Отмена заказа {order_id} (оплата не прошла)")
         await release_item(order_id, item_id, quantity)  
         await update_order_status(order_id, "failed")
         return False
 
-    # 3. Отправляем заказ
+    
     if not await ship_order(order_id):  
         logger.error(f"❌ Отмена заказа {order_id} (ошибка при отправке)")
         await compensate_payment(order_id)  
